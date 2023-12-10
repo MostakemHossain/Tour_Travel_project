8a93a6a49f1d454ca3028da80d4e6445ec1a06fd
MostakemHossain <mostakememon123@gmail.com>
2023-12-10 14:44:51 +0600

    Tour and travel project initilize

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+
+
+async function main() {
+    await mongoose.connect(config.database_url as string);
+  
+    app.listen(config.port, () => {
+        console.log(`Example app listening at http://localhost:${config.port}`)
+      })
+  }
+
+  main();
+
+
