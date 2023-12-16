bfb8f56d1c34d37df3a7d0ee9f495378c6d5a2d1
MostakemHossain <mostakememon123@gmail.com>
2023-12-16 10:46:41 +0600

    validation error modified

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,14 +1,46 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { TErrorResponse } from '../types/TErrorResponse';
 
-export const globalErrorHandler= (err:any,req:Request,res:Response,next:NextFunction)=>{
-    const statusCode = res.statusCode || 500;
-    const message= err.message || 'Something went wrong';
-    const status=err.status || 'error';
-    res.status(statusCode).json({
-        status ,
-        message,
-    })
-}
+export const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  // const statusCode = res.statusCode || 500;
+  // const message= err.message || 'Something went wrong';
+  // const status=err.status || 'error';
+
+  const errorResponse: TErrorResponse = {
+    statusCode: err.statusCode,
+    status: err.status || 'error',
+    message: err.message || 'Something went wrong',
+    issues: err.issues || [],
+  };
+
+  if (err instanceof mongoose.Error.ValidationError) {
+    errorResponse.statusCode = 400;
+    errorResponse.message = "validation Error";
+    errorResponse.status = 'error';
+    const errorValues = Object.values(err.errors);
+    errorValues.forEach(
+      (errObj: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
+        errorResponse.issues.push({
+          path: errObj.path,
+          message: errObj.message,
+        });
+      },
+    );
+  }
+
+  res.status(errorResponse.statusCode).json({
+    status: errorResponse.status,
+    message: errorResponse.message,
+    issues: errorResponse.issues,
+    // err: err,
+  });
+};
